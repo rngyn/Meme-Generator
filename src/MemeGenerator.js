e957c2fd0fe6366d848370b0379159e05092abb8
Robert Nguyen <rhngyn@gmail.com>
2021-08-28 11:23:19 -0600

    Fixed compiling errors

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -54,8 +54,8 @@ class MemeGenerator extends Component {
                 </form>
                 <div className="meme">
                     <img src={this.state.randomImg} alt="" />
-                    <h2 className="top">{this.state.topText</h2>
-                    <h2 className="bottom">{this.state.bottomText</h2>
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
                 </div>
             </div>
         )
